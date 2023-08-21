1602c567b744f5e1afce1d288134ba0ef2cd7439
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2023-08-21 14:09:28 +0300

    lesson1

--- src/main/java/de/telran/lesson1/Main.java
+++ src/main/java/de/telran/lesson1/Main.java
@@ -1,4 +1,4 @@
-package de.telran;
+package de.telran.lesson1;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
--- src/main/java/de/telran/lesson1/practice/Task1.java
+++ src/main/java/de/telran/lesson1/practice/Task1.java
@@ -1,2 +1,13 @@
-package de.telran.lesson1.practice;public class Task1 {
+package de.telran.lesson1.practice;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Alex!");
+
+        System.out.print("Hello, ");
+        System.out.print("my ");
+        System.out.print("name ");
+        System.out.print("is ");
+        System.out.println("Alex!");
+    }
 }
